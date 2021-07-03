522dfa1f91bd9d92a2cf6f6eb4f7361ab369e4fb
planti <ivan.maffioli@pm.me>
2021-07-03 15:58:41 +0200

    Problem 2

--- Program.fs
+++ Program.fs
@@ -2,7 +2,10 @@
 [<EntryPoint>]
 let main argv =
     
-    Problem1.explicitRcursiveSolution
-    Problem1.listSolution
+    //
+    //Problem1.explicitRcursiveSolution
+    //Problem1.listSolution
+    Problem2.listSolution
+    Problem2.binetSolution
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
